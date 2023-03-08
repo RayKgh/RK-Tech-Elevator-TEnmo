b27cd13e53f01808fd00e0f2898bde70c2f9fb86
Jake Arnett <Jake.Arnett@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[a[D[D[D[D[j[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D>
2023-03-08 10:38:21 -0500

    added tables in tenmo.sql file

--- capstone/database/tenmo.sql
+++ capstone/database/tenmo.sql
@@ -1,5 +1,7 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS transaction;
+DROP TABLE IF EXISTS account;
 DROP TABLE IF EXISTS tenmo_user;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
@@ -16,5 +18,25 @@ CREATE TABLE tenmo_user (
 	CONSTRAINT PK_tenmo_user PRIMARY KEY (user_id),
 	CONSTRAINT UQ_username UNIQUE (username)
 );
+
+CREATE TABLE account ( 
+	account_id serial PRIMARY KEY,
+	user_id int UNIQUE NOT NULL,
+	balance decimal (11,2) DEFAULT 1000.00 ,
+	CONSTRAINT FK_account_user FOREIGN KEY(user_id) REFERENCES tenmo_user(user_id)
+);
+
+CREATE TABLE transaction (
+	transaction_id serial PRIMARY KEY,
+	sender_id int NOT NULL,
+	recipient_id int NOT NULL,
+	transfer_amt decimal NOT NULL,
+	timestamp timestamp NOT NULL,
+	is_requested boolean NOT NULL,
+	CONSTRAINT FK_account_sender FOREIGN KEY(sender_id) REFERENCES account(user_id),
+	CONSTRAINT FK_account_recipient FOREIGN KEY(recipient_id) REFERENCES account(user_id)
+);
+
+
 
 COMMIT;
